bf1355d4f60a2c10844f375ceb45d04325509955
kyarachoo <139100157+kyarachoo@users.noreply.github.com>
2026-09-14 07:24:21 -0400

    Create load_synthea.py
    Loading synthetic data (csv) from Synthea

---
+++ src/load_synthea.py
@@ -0,0 +1,20 @@
+patients = pd.read_csv('data/synthea/patients.csv')
+encounters = pd.read_csv('data/synthea/encounters.csv')
+conditions = pd.read_csv('data/synthea/conditions.csv')
+medications = pd.read_csv('data/synthea/medications.csv')
+
+print('PATIENTS')
+print(patients.shape)
+print(patients.columns.tolist())
+
+print('\nENCOUNTERS')
+print(encounters.shape)
+print(encounters.columns.tolist())
+
+print('\nCONDITIONS')
+print(conditions.shape)
+print(conditions.columns.tolist())
+
+print('\nMEDICATIONS')
+print(medications.shape)
+print(medications.columns.tolist())
